635a34f7eb7758179ccd79528f37348dbe680569
Ching Kao <0980124jim@gmail.com>
2025-10-21 15:07:10 -0700

    feat: m2 part1

---
+++ apps/m2/task2.py
@@ -0,0 +1,22 @@
+import sys
+from bs4 import BeautifulSoup, SoupStrainer
+
+file_name = sys.argv[1]
+only_a_tags = SoupStrainer("a")
+
+
+def get_parser(file_name):
+    if file_name.endswith(".html"):
+        return "lxml"
+    elif file_name.endswith(".xml"):
+        return "lxml-xml"
+    else:
+        raise ValueError("Unsupported file extension: {}".format(file_name))
+
+
+with open(file_name) as fr:
+    file_content = fr.read()
+    soup = BeautifulSoup(file_content, get_parser(file_name), parse_only=only_a_tags)
+
+
+print(soup.prettify())
---
+++ apps/m2/task4.py
@@ -0,0 +1,26 @@
+# Print out all the tags in the document.
+import sys
+from bs4 import BeautifulSoup
+from bs4.filter import SoupStrainer
+
+file_name = sys.argv[1]
+parser = ""
+only_tag_with_id = SoupStrainer(id=True)
+
+
+def get_parser(file_name):
+    if file_name.endswith(".html"):
+        return "lxml"
+    elif file_name.endswith(".xml"):
+        return "lxml-xml"
+    else:
+        raise ValueError("Unsupported file extension: {}".format(file_name))
+
+
+with open(file_name) as fr:
+    file_content = fr.read()
+    soup = BeautifulSoup(
+        file_content, get_parser(file_name), parse_only=only_tag_with_id
+    )
+
+print(soup.prettify())
